ca7ca237b3914bd35ecec2e43a311233b8fc10e0
dylvaz <vazquezadylan@gmail.com>
2021-09-19 20:52:16 -0700

    init

---
+++ frontend/src/pages/Cart/Cart.js
@@ -0,0 +1,11 @@
+import './Cart.css';
+
+const Cart = () => {
+  return (
+    <div className='cart-page'>
+      <h1>Hello from Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home-page'>
+      <h1>Hello from Home</h1>
+    </div>
+  );
+};
+
+export default Home;
