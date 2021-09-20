6b6041b359a7991cf4318f24a3e0341e98faceec
dylvaz <vazquezadylan@gmail.com>
2021-09-19 17:55:24 -0700

    added the functions to be used for /api/products route

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const app = require('express')();
 
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
 
 const PORT = process.env.PORT || 7654;
 
 connectDB();
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, (err) => {
   if (err) {
