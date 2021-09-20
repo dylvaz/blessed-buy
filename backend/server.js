7ecefcc29350d2e58fb057aa84b85844df0f713b
dylvaz <vazquezadylan@gmail.com>
2021-09-19 20:52:27 -0700

    added morgan for logging

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
-const app = require('express')();
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
 
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 
 const PORT = process.env.PORT || 7654;
+
+app.use(morgan('dev'));
 
 connectDB();
 
