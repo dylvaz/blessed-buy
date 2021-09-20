cb9c9b488f8c8eab5a10efc5a0a16b011aff98d3
dylvaz <vazquezadylan@gmail.com>
2021-09-19 17:56:10 -0700

    added routing depending on which route is hit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {
+  getAllProducts,
+  getProductById,
+} = require('../controller/productController');
+
+//@desc GET all products from db
+//@route GET /api/products
+//@access Public
+router.get('/', getAllProducts);
+
+//@desc GET a product by id from db
+//@route GET /api/products/:id
+//@access Public
+router.get('/:id', getProductById);
+
+module.exports = router;
