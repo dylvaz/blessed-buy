059108e9687cf837d0faebb3bc9a88ba8b48219b
dylvaz <vazquezadylan@gmail.com>
2024-03-24 05:33:01 -0700

    added dotenv/config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
